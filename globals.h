8853af5bc19f4b39f3c50efca94c5ff98c43a7e7
Seung <separk@pugetsound.edu>
2022-04-05 01:52:05 -0400

    added semaphores in globals.h

--- globals.h
+++ globals.h
@@ -11,6 +11,14 @@
 int num_threads;	// number of customer threads
 int now_serving;	// customer's ID who is being served
 
-//TODO - declare some semaphores
+/*  semaphores  */
+sem_t* bar_empty;
+sem_t* customer_inside;
+sem_t* bartender_available;
+sem_t* customer_places_order;
+sem_t* bartender_made_drink;
+sem_t* customer_paid;
+sem_t* bartender_confirms_payment;
+sem_t* customer_leaves;
 
 #endif /* GLOBALS_H_ */
